37ee3e45f81296f98b93a3c071f450289b43ec00
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-10-08 13:16:40 -0400

    Update for line breaks

--- create_seurat_object.sh
+++ create_seurat_object.sh
@@ -24,6 +24,6 @@ cd3col4="$16"
 cd3col5="$17"
 cd3col6="$18"
 
-Rscript create_seurat_object.R $cd1col1 $cd1col2 $cd1col3 $cd1col4 $cd1col5 $cd1col6
-                        $cd2col1 $cd2col2 $cd2col3 $cd2col4 $cd2col5 $cd2col6
+Rscript create_seurat_object.R $cd1col1 $cd1col2 $cd1col3 $cd1col4 $cd1col5 $cd1col6 \
+                        $cd2col1 $cd2col2 $cd2col3 $cd2col4 $cd2col5 $cd2col6 \
                         $cd3col1 $cd3col2 $cd3col3 $cd3col4 $cd3col5 $cd3col6
--- seurat_analysis.sh
+++ seurat_analysis.sh
@@ -25,7 +25,7 @@ cd3col6='/project2/gilad/reem/singlecellCM/round1/fulldata/CD3/CD3col6/output/dg
 
 # PART 1: Create Seurat Object from raw data files
 if true; then
-   sbatch create_seurat_object.sh $cd1col1 $cd1col2 $cd1col3 $cd1col4 $cd1col5 $cd1col6
-                                    $cd2col1 $cd2col2 $cd2col3 $cd2col4 $cd2col5 $cd2col6
+   sbatch create_seurat_object.sh $cd1col1 $cd1col2 $cd1col3 $cd1col4 $cd1col5 $cd1col6 \
+                                    $cd2col1 $cd2col2 $cd2col3 $cd2col4 $cd2col5 $cd2col6 \
                                     $cd3col1 $cd3col2 $cd3col3 $cd3col4 $cd3col5 $cd3col6
 fi
