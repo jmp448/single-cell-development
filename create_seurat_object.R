298d5ac6bf55f4a9fb101c7e8875086da7ff3db5
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-10-08 19:41:15 -0400

    Print error messages

--- create_seurat_object.R
+++ create_seurat_object.R
@@ -9,6 +9,7 @@ library(Seurat)
 rawdata <- c()
 for (i in 1:18) {
   rawdata <- c(rawdata, args[i])
+  print(rawdata)
 }
 
 ## Combine all 18 collections into a single Seurat object First, read in the raw
