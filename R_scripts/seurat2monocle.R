c9f1b68bb668e184543e8a7ea21dee3658ed004e
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-10-30 00:23:21 -0400

    Update to properly include scaled log normalized values

--- R_scripts/seurat2monocle.R
+++ R_scripts/seurat2monocle.R
@@ -1,6 +1,7 @@
 library(Seurat)
 library(monocle3)
 
+# Create monocle object from just the straight seurat data (will need to be preprocessed in monocle)
 seurat_file <- "./rds_objects/seurat_obj_lowpass.RDS"
 sc_seurat <- readRDS(seurat_file)
 expression_matrix <- sc_seurat@assays[["RNA"]]@counts
@@ -16,9 +17,10 @@ monocle <- estimate_size_factors(monocle)
 saveRDS(monocle, "./rds_objects/monocle_from_seurat_lowpass_unprocessed.RDS")
 rm(list=ls())
 
+# Create monocle object from the log normalized seurat data
 seurat_file <- "./rds_objects/seurat_obj_lowpass_basic_norm.RDS"
 sc_seurat <- readRDS(seurat_file)
-expression_matrix <- sc_seurat@assays[["RNA"]]@data
+expression_matrix <- sc_seurat@assays[["RNA"]]@scale.data
 cell_metadata <- sc_seurat[[]]
 gene_metadata <- as.data.frame(rownames(expression_matrix))
 rownames(gene_metadata) <- rownames(expression_matrix)
@@ -27,10 +29,10 @@ monocle <- new_cell_data_set(expression_data = expression_matrix,
                              gene_metadata = gene_metadata,
                              cell_metadata = cell_metadata)
 monocle <- monocle[,Matrix::colSums(exprs(monocle)) != 0]
-monocle <- estimate_size_factors(monocle)
 saveRDS(monocle, "./rds_objects/monocle_from_seurat_lowpass_basic_norm.RDS")
 rm(list=ls())
 
+# Create monocle object from the log normalized seurat data but with no scale factor
 seurat_file <- "./rds_objects/seurat_obj_lowpass_noscale_norm.RDS"
 sc_seurat <- readRDS(seurat_file)
 expression_matrix <- sc_seurat@assays[["RNA"]]@scale.data
@@ -42,10 +44,10 @@ monocle <- new_cell_data_set(expression_data = expression_matrix,
                              gene_metadata = gene_metadata,
                              cell_metadata = cell_metadata)
 monocle <- monocle[,Matrix::colSums(exprs(monocle)) != 0]
-monocle <- estimate_size_factors(monocle)
 saveRDS(monocle, "./rds_objects/monocle_from_seurat_lowpass_noscale_norm.RDS")
 rm(list=ls())
 
+# Create monocle object from SC transformed seurat object
 seurat_file <- "./rds_objects/seurat_obj_lowpass_sctransform.RDS"
 sc_seurat <- readRDS(seurat_file)
 expression_matrix <- sc_seurat@assays[["SCT"]]@scale.data
@@ -57,10 +59,10 @@ monocle <- new_cell_data_set(expression_data = expression_matrix,
                              gene_metadata = gene_metadata,
                              cell_metadata = cell_metadata)
 monocle <- monocle[,Matrix::colSums(exprs(monocle)) != 0]
-monocle <- estimate_size_factors(monocle)
 saveRDS(monocle, "./rds_objects/monocle_from_seurat_lowpass_sctransform.RDS")
 rm(list=ls())
 
+# Create monocle object from SC transformed seurat object with mito regressed out
 seurat_file <- "./rds_objects/seurat_obj_lowpass_sctransform_mitoregress.RDS"
 sc_seurat <- readRDS(seurat_file)
 expression_matrix <- sc_seurat@assays[["RNA"]]@scale.data
@@ -72,10 +74,10 @@ monocle <- new_cell_data_set(expression_data = expression_matrix,
                              gene_metadata = gene_metadata,
                              cell_metadata = cell_metadata)
 monocle <- monocle[,Matrix::colSums(exprs(monocle)) != 0]
-monocle <- estimate_size_factors(monocle)
 saveRDS(monocle, "./rds_objects/monocle_from_seurat_lowpass_sctransform_mitoregress.RDS")
 rm(list=ls())
 
+# Create monocle object from SC transformed seurat object with mito and colday regressed out 
 seurat_file <- "./rds_objects/seurat_obj_lowpass_sctransform_mitocoldayregress.RDS"
 sc_seurat <- readRDS(seurat_file)
 expression_matrix <- sc_seurat@assays[["RNA"]]@scale.data
@@ -87,6 +89,5 @@ monocle <- new_cell_data_set(expression_data = expression_matrix,
                              gene_metadata = gene_metadata,
                              cell_metadata = cell_metadata)
 monocle <- monocle[,Matrix::colSums(exprs(monocle)) != 0]
-monocle <- estimate_size_factors(monocle)
 saveRDS(monocle, "./rds_objects/monocle_from_seurat_sctransform_mitocoldayregress.RDS")
 rm(list=ls())
