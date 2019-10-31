66821b4af4b999a0af2c29224601c44f387d728f
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-10-31 16:09:59 -0400

    Update for dim reduction

--- R_scripts/seurat_visualizations.R
+++ R_scripts/seurat_visualizations.R
@@ -1,5 +1,52 @@
 library(Seurat)
 
+plots_loc <- "./seurat_plots/"
+
+
+### SCTransform
+sc <- readRDS("./rds_objects/old_seurat_obj_lowpass_processed.rds")
+
+# PCA diffday
+png(paste0(plots_loc, "old_lowpass_sctransform_PCA_diffday.png"), width=800, height=600)
+PCAPlot(sc, group.by="diffday")
+dev.off()
+
+# PCA individual
+png(paste0(plots_loc, "old_lowpass_sctransform_PCA_indiv.png"), width=800, height=600)
+PCAPlot(sc, group.by="individual")
+dev.off()
+
+# PCA collection
+png(paste0(plots_loc, "old_lowpass_sctransform_PCA_col.png"), width=800, height=600)
+PCAPlot(sc, group.by="orig.ident")
+dev.off()
+
+# PCA colday
+png(paste0(plots_loc, "old_lowpass_sctransform_PCA_colday.png"), width=800, height=600)
+PCAPlot(sc, group.by="colday")
+dev.off()
+
+# UMAP diffday
+png(paste0(plots_loc, "old_lowpass_sctransform_UMAP_diffday.png"), width=800, height=600)
+DimPlot(sc, group.by="diffday")
+dev.off()
+
+# UMAP individual
+png(paste0(plots_loc, "old_lowpass_sctransform_UMAP_indiv.png"), width=800, height=600)
+DimPlot(sc, group.by="individual")
+dev.off()
+
+# UMAP collection
+png(paste0(plots_loc, "old_lowpass_sctransform_UMAP_col.png"), width=800, height=600)
+DimPlot(sc, group.by="orig.ident")
+dev.off()
+
+# UMAP colday
+png(paste0(plots_loc, "old_lowpass_sctransform_UMAP_colday.png"), width=800, height=600)
+DimPlot(sc, group.by="colday")
+dev.off()
+
+
 # plots_loc <- "./seurat_plots/"
 #
 # ### Basic norm
@@ -93,146 +140,146 @@ library(Seurat)
 #
 # rm(list=ls())
 
-plots_loc <- "./seurat_plots/"
-
-
-### SCTransform
-sc <- readRDS("./rds_objects/seurat_obj_lowpass_sctransform_processed.RDS")
-
-# PCA diffday
-png(paste0(plots_loc, "lowpass_sctransform_PCA_diffday.png"), width=800, height=600)
-PCAPlot(sc, group.by="diffday")
-dev.off()
-
-# PCA individual
-png(paste0(plots_loc, "lowpass_sctransform_PCA_indiv.png"), width=800, height=600)
-PCAPlot(sc, group.by="individual")
-dev.off()
-
-# PCA collection
-png(paste0(plots_loc, "lowpass_sctransform_PCA_col.png"), width=800, height=600)
-PCAPlot(sc, group.by="orig.ident")
-dev.off()
-
-# PCA colday
-png(paste0(plots_loc, "lowpass_sctransform_PCA_colday.png"), width=800, height=600)
-PCAPlot(sc, group.by="colday")
-dev.off()
-
-# UMAP diffday
-png(paste0(plots_loc, "lowpass_sctransform_UMAP_diffday.png"), width=800, height=600)
-DimPlot(sc, group.by="diffday")
-dev.off()
-
-# UMAP individual
-png(paste0(plots_loc, "lowpass_sctransform_UMAP_indiv.png"), width=800, height=600)
-DimPlot(sc, group.by="individual")
-dev.off()
-
-# UMAP collection
-png(paste0(plots_loc, "lowpass_sctransform_UMAP_col.png"), width=800, height=600)
-DimPlot(sc, group.by="orig.ident")
-dev.off()
-
-# UMAP colday
-png(paste0(plots_loc, "lowpass_sctransform_UMAP_colday.png"), width=800, height=600)
-DimPlot(sc, group.by="colday")
-dev.off()
-
-rm(list=ls())
-
-plots_loc <- "./seurat_plots/"
-
-
-### SCTransform Mitoregress
-sc <- readRDS("./rds_objects/seurat_obj_lowpass_sctransform_mitoregress_processed.RDS")
-
-# PCA diffday
-png(paste0(plots_loc, "lowpass_sctransform_mitoregress_PCA_diffday.png"), width=800, height=600)
-PCAPlot(sc, group.by="diffday")
-dev.off()
-
-# PCA individual
-png(paste0(plots_loc, "lowpass_sctransform_mitoregress_PCA_indiv.png"), width=800, height=600)
-PCAPlot(sc, group.by="individual")
-dev.off()
-
-# PCA collection
-png(paste0(plots_loc, "lowpass_sctransform_mitoregress_PCA_col.png"), width=800, height=600)
-PCAPlot(sc, group.by="orig.ident")
-dev.off()
-
-# PCA colday
-png(paste0(plots_loc, "lowpass_sctransform_mitoregress_PCA_colday.png"), width=800, height=600)
-PCAPlot(sc, group.by="colday")
-dev.off()
-
-# UMAP diffday
-png(paste0(plots_loc, "lowpass_sctransform_mitoregress_UMAP_diffday.png"), width=800, height=600)
-DimPlot(sc, group.by="diffday")
-dev.off()
-
-# UMAP individual
-png(paste0(plots_loc, "lowpass_sctransform_mitoregress_UMAP_indiv.png"), width=800, height=600)
-DimPlot(sc, group.by="individual")
-dev.off()
-
-# UMAP collection
-png(paste0(plots_loc, "lowpass_sctransform_mitoregress_UMAP_col.png"), width=800, height=600)
-DimPlot(sc, group.by="orig.ident")
-dev.off()
-
-# UMAP colday
-png(paste0(plots_loc, "lowpass_sctransform_mitoregress_UMAP_colday.png"), width=800, height=600)
-DimPlot(sc, group.by="colday")
-dev.off()
-
-rm(list=ls())
-
-plots_loc <- "./seurat_plots/"
-
-
-### SCTransform Mito Colday Regress
-sc <- readRDS("./rds_objects/seurat_obj_lowpass_sctransform_mitocoldayregress_processed.RDS")
-
-# PCA diffday
-png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_diffday.png"), width=800, height=600)
-PCAPlot(sc, group.by="diffday")
-dev.off()
-
-# PCA individual
-png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_indiv.png"), width=800, height=600)
-PCAPlot(sc, group.by="individual")
-dev.off()
-
-# PCA collection
-png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_col.png"), width=800, height=600)
-PCAPlot(sc, group.by="orig.ident")
-dev.off()
-
-# PCA colday
-png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_colday.png"), width=800, height=600)
-PCAPlot(sc, group.by="colday")
-dev.off()
-
-# UMAP diffday
-png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_diffday.png"), width=800, height=600)
-DimPlot(sc, group.by="diffday")
-dev.off()
-
-# UMAP individual
-png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_indiv.png"), width=800, height=600)
-DimPlot(sc, group.by="individual")
-dev.off()
-
-# UMAP collection
-png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_col.png"), width=800, height=600)
-DimPlot(sc, group.by="orig.ident")
-dev.off()
-
-# UMAP colday
-png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_colday.png"), width=800, height=600)
-DimPlot(sc, group.by="colday")
-dev.off()
-
-rm(list=ls())
+# plots_loc <- "./seurat_plots/"
+#
+#
+# ### SCTransform
+# sc <- readRDS("./rds_objects/seurat_obj_lowpass_sctransform_processed.RDS")
+#
+# # PCA diffday
+# png(paste0(plots_loc, "lowpass_sctransform_PCA_diffday.png"), width=800, height=600)
+# PCAPlot(sc, group.by="diffday")
+# dev.off()
+#
+# # PCA individual
+# png(paste0(plots_loc, "lowpass_sctransform_PCA_indiv.png"), width=800, height=600)
+# PCAPlot(sc, group.by="individual")
+# dev.off()
+#
+# # PCA collection
+# png(paste0(plots_loc, "lowpass_sctransform_PCA_col.png"), width=800, height=600)
+# PCAPlot(sc, group.by="orig.ident")
+# dev.off()
+#
+# # PCA colday
+# png(paste0(plots_loc, "lowpass_sctransform_PCA_colday.png"), width=800, height=600)
+# PCAPlot(sc, group.by="colday")
+# dev.off()
+#
+# # UMAP diffday
+# png(paste0(plots_loc, "lowpass_sctransform_UMAP_diffday.png"), width=800, height=600)
+# DimPlot(sc, group.by="diffday")
+# dev.off()
+#
+# # UMAP individual
+# png(paste0(plots_loc, "lowpass_sctransform_UMAP_indiv.png"), width=800, height=600)
+# DimPlot(sc, group.by="individual")
+# dev.off()
+#
+# # UMAP collection
+# png(paste0(plots_loc, "lowpass_sctransform_UMAP_col.png"), width=800, height=600)
+# DimPlot(sc, group.by="orig.ident")
+# dev.off()
+#
+# # UMAP colday
+# png(paste0(plots_loc, "lowpass_sctransform_UMAP_colday.png"), width=800, height=600)
+# DimPlot(sc, group.by="colday")
+# dev.off()
+#
+# rm(list=ls())
+#
+# plots_loc <- "./seurat_plots/"
+#
+#
+# ### SCTransform Mitoregress
+# sc <- readRDS("./rds_objects/seurat_obj_lowpass_sctransform_mitoregress_processed.RDS")
+#
+# # PCA diffday
+# png(paste0(plots_loc, "lowpass_sctransform_mitoregress_PCA_diffday.png"), width=800, height=600)
+# PCAPlot(sc, group.by="diffday")
+# dev.off()
+#
+# # PCA individual
+# png(paste0(plots_loc, "lowpass_sctransform_mitoregress_PCA_indiv.png"), width=800, height=600)
+# PCAPlot(sc, group.by="individual")
+# dev.off()
+#
+# # PCA collection
+# png(paste0(plots_loc, "lowpass_sctransform_mitoregress_PCA_col.png"), width=800, height=600)
+# PCAPlot(sc, group.by="orig.ident")
+# dev.off()
+#
+# # PCA colday
+# png(paste0(plots_loc, "lowpass_sctransform_mitoregress_PCA_colday.png"), width=800, height=600)
+# PCAPlot(sc, group.by="colday")
+# dev.off()
+#
+# # UMAP diffday
+# png(paste0(plots_loc, "lowpass_sctransform_mitoregress_UMAP_diffday.png"), width=800, height=600)
+# DimPlot(sc, group.by="diffday")
+# dev.off()
+#
+# # UMAP individual
+# png(paste0(plots_loc, "lowpass_sctransform_mitoregress_UMAP_indiv.png"), width=800, height=600)
+# DimPlot(sc, group.by="individual")
+# dev.off()
+#
+# # UMAP collection
+# png(paste0(plots_loc, "lowpass_sctransform_mitoregress_UMAP_col.png"), width=800, height=600)
+# DimPlot(sc, group.by="orig.ident")
+# dev.off()
+#
+# # UMAP colday
+# png(paste0(plots_loc, "lowpass_sctransform_mitoregress_UMAP_colday.png"), width=800, height=600)
+# DimPlot(sc, group.by="colday")
+# dev.off()
+#
+# rm(list=ls())
+#
+# plots_loc <- "./seurat_plots/"
+#
+#
+# ### SCTransform Mito Colday Regress
+# sc <- readRDS("./rds_objects/seurat_obj_lowpass_sctransform_mitocoldayregress_processed.RDS")
+#
+# # PCA diffday
+# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_diffday.png"), width=800, height=600)
+# PCAPlot(sc, group.by="diffday")
+# dev.off()
+#
+# # PCA individual
+# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_indiv.png"), width=800, height=600)
+# PCAPlot(sc, group.by="individual")
+# dev.off()
+#
+# # PCA collection
+# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_col.png"), width=800, height=600)
+# PCAPlot(sc, group.by="orig.ident")
+# dev.off()
+#
+# # PCA colday
+# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_colday.png"), width=800, height=600)
+# PCAPlot(sc, group.by="colday")
+# dev.off()
+#
+# # UMAP diffday
+# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_diffday.png"), width=800, height=600)
+# DimPlot(sc, group.by="diffday")
+# dev.off()
+#
+# # UMAP individual
+# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_indiv.png"), width=800, height=600)
+# DimPlot(sc, group.by="individual")
+# dev.off()
+#
+# # UMAP collection
+# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_col.png"), width=800, height=600)
+# DimPlot(sc, group.by="orig.ident")
+# dev.off()
+#
+# # UMAP colday
+# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_colday.png"), width=800, height=600)
+# DimPlot(sc, group.by="colday")
+# dev.off()
+#
+# rm(list=ls())
