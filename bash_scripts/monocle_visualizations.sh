a65dcd8862be1789d262e6e22f76d46ab58e1e14
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-11-01 14:59:32 -0400

    Fix capitalization, back to the top

--- bash_scripts/monocle_visualizations.sh
+++ bash_scripts/monocle_visualizations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=02:00:00
 #SBATCH --mem=40G
-#SBATCH --output=./error_files/monocle_visualization.out
-#SBATCH --error=./error_files/monocle_visualization.err
+#SBATCH --output=./error_files/monocle_visualizations.out
+#SBATCH --error=./error_files/monocle_visualizations.err
 #SBATCH --job-name=monocle_visualization
 
 module load R
