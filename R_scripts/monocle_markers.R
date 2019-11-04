8ddde262cb250deb911a1f829213b88694f4b7d7
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-11-04 10:46:09 -0500

    clean up names, only use clear markers

--- R_scripts/monocle_markers.R
+++ R_scripts/monocle_markers.R
@@ -6,48 +6,32 @@ cds <- readRDS("./rds_objects/monocle_from_seurat_lowpass_sctransform_mitoregres
 
 # Check out expression for specific genes
 # Stem cell markers
-stem_cell_markers <- c("EPCAM", "LIN28A", "ZFP42")
-png("./monocle_plots/stem_cell_markers.png", width=800, height=600)
+stem_cell_markers <- c("EPCAM")
+png("./monocle_plots/markers_stem_cell.png", width=800, height=600)
 plot_cells(cds, genes=stem_cell_markers,
            label_cell_groups=FALSE,
            show_trajectory_graph=FALSE)
 dev.off()
 
 # Mesoderm
-mesoderm_markers <- c("CXCR4", "KDR", "EOMES", "FOXF1", "MIXL1", "TBX6")
-png("./monocle_plots/mesoderm_markers.png", width=800, height=600)
+mesoderm_markers <- c("MIXL1")
+png("./monocle_plots/markers_mesoderm.png", width=800, height=600)
 plot_cells(cds, genes=mesoderm_markers,
            label_cell_groups=FALSE,
            show_trajectory_graph=FALSE)
 dev.off()
 
-# Cardiac progenitor to cardiomyocyte transition
-progenitor_markers <- c("KDR", "ATF2", "GATA2", "MEF2C", "TBX5", "WT1")
-png("./monocle_plots/progenitor_markers.png", width=800, height=600)
-plot_cells(cds, genes=progenitor_markers,
-           label_cell_groups=FALSE,
-           show_trajectory_graph=FALSE)
-dev.off()
-
 # Cardiomyocyte
-cardiomyocyte_markers <- c("TNNT2", "TNNI1", "GJA1", "VCAM1", "TNNC1", "MYH7")
-png("./monocle_plots/cardiomyocyte_markers.png", width=800, height=600)
+cardiomyocyte_markers <- c("TNNT2", "TNNI1", "TNNC1")
+png("./monocle_plots/markers_cardiomyocyte.png", width=1800, height=600)
 plot_cells(cds, genes=cardiomyocyte_markers,
-           label_cell_groups=FALSE,
-           show_trajectory_graph=FALSE)
-dev.off()
-
-# Myofibroblast
-myofibroblast_markers <- "ACTA2"
-png("./monocle_plots/myofibroblast_markers.png", width=800, height=600)
-plot_cells(cds, genes=myofibroblast_markers,
            label_cell_groups=FALSE,
            show_trajectory_graph=FALSE)
 dev.off()
 
 # Epicardium
 epicardium_markers = "IGF2"
-png("./monocle_plots/epicardium_markers.png", width=800, height=600)
+png("./monocle_plots/markers_epicardium.png", width=800, height=600)
 plot_cells(cds, genes=epicardium_markers,
            label_cell_groups=FALSE,
            show_trajectory_graph=FALSE)
@@ -55,7 +39,7 @@ dev.off()
 
 # Fibroblast
 fibroblast_markers <- c("VIM", "S100A4", "COL1A2", "COL3A1", "TSLP")
-png("./monocle_plots/fibroblast_markers.png", width=800, height=600)
+png("./monocle_plots/fibroblast_markers.png", width=1800, height=600)
 plot_cells(cds, genes=fibroblast_markers,
            label_cell_groups=FALSE,
            show_trajectory_graph=FALSE)
