3f10a58933ac1b95fc73aa7359a73585755b6ff3
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-11-04 12:56:01 -0500

    Monocle trajectory

--- R_scripts/monocle_trajectory.R
+++ R_scripts/monocle_trajectory.R
@@ -1,8 +1,18 @@
 library(monocle3)
 
-m <- readRDS("./rds_objects/monocle_from_seurat_lowpass_sctransform_mitoregress_processed.RDS")
-saveRDS(m, "./rds_objects/monocle_from_seurat_lowpass_sctransform_mitoregress_graphed.RDS")
-rm(list=ls())
+cds <- readRDS("./rds_objects/monocle_from_seurat_lowpass_sctransform_mitoregress_processed.RDS")
 
-m <- readRDS("./rds_objects/monocle_from_seurat_lowpass_sctransform_mitocoldayregress_processed.RDS")
-saveRDS(m, "./rds_objects/monocle_from_seurat_lowpass_sctransform_mitocoldayregress_graphed.RDS")
+library(monocle3)
+
+cds=cluster_cells(cds, resolution=1e-7)
+plt <- plot_cells(cds)
+print(plt)
+plt <- plot_cells(cds, color_cells_by="partition", group_cells_by="partition")
+print(plt)
+cds <- learn_graph(cds)
+plot_cells(cds,
+           color_cells_by = "diffday",
+           label_groups_by_cluster=F,
+           label_leaves=F,
+           label_branch_points=F)
+cds <- order_cells(cds)
