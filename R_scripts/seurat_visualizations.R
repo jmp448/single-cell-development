dc1c9cc67b7e027fad195405f0bda2149c5ff1af
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-11-03 20:58:14 -0500

    Multiple processing

--- R_scripts/seurat_visualizations.R
+++ R_scripts/seurat_visualizations.R
@@ -1,130 +1,130 @@
 library(Seurat)
 
-# plots_loc <- "./seurat_plots/"
-#
-# ### Basic norm
-# sc <- readRDS("./rds_objects/seurat_obj_lowpass_basic_norm_processed.RDS")
-#
-# png(paste0(plots_loc, "lowpass_basic_norm_PCA_diffday.png"), width=800, height=600)
-# PCAPlot(sc, group.by="diffday")
-# dev.off()
-# png(paste0(plots_loc, "lowpass_basic_norm_PCA_indiv.png"), width=800, height=600)
-# PCAPlot(sc, group.by="individual")
-# dev.off()
-# png(paste0(plots_loc, "lowpass_basic_norm_PCA_col.png"), width=800, height=600)
-# PCAPlot(sc, group.by="orig.ident")
-# dev.off()
-# png(paste0(plots_loc, "lowpass_basic_norm_PCA_colday.png"), width=800, height=600)
-# PCAPlot(sc, group.by="colday")
-# dev.off()
-#
-# png(paste0(plots_loc, "lowpass_basic_norm_UMAP_diffday.png"), width=800, height=600)
-# DimPlot(sc, group.by="diffday")
-# dev.off()
-# png(paste0(plots_loc, "lowpass_basic_norm_UMAP_indiv.png"), width=800, height=600)
-# DimPlot(sc, group.by="individual")
-# dev.off()
-# png(paste0(plots_loc, "lowpass_basic_norm_UMAP_col.png"), width=800, height=600)
-# DimPlot(sc, group.by="orig.ident")
-# dev.off()
-# png(paste0(plots_loc, "lowpass_basic_norm_UMAP_colday.png"), width=800, height=600)
-# DimPlot(sc, group.by="colday")
-# dev.off()
-# rm(list=ls())
-#
-# plots_loc <- "./seurat_plots/"
-# ### Noscale norm
-# sc <- readRDS("./rds_objects/seurat_obj_lowpass_noscale_norm_processed.RDS")
-#
-# # PCA diffday
-# png(paste0(plots_loc, "lowpass_noscale_norm_PCA_diffday.png"), width=800, height=600)
-# PCAPlot(sc, group.by="diffday")
-# dev.off()
-#
-# # PCA individual
-# png(paste0(plots_loc, "lowpass_noscale_norm_PCA_indiv.png"), width=800, height=600)
-# PCAPlot(sc, group.by="individual")
-# dev.off()
-#
-# # PCA collection
-# png(paste0(plots_loc, "lowpass_noscale_norm_PCA_col.png"), width=800, height=600)
-# PCAPlot(sc, group.by="orig.ident")
-# dev.off()
-#
-# # PCA colday
-# png(paste0(plots_loc, "lowpass_noscale_norm_PCA_colday.png"), width=800, height=600)
-# PCAPlot(sc, group.by="colday")
-# dev.off()
-#
-# # UMAP diffday
-# png(paste0(plots_loc, "lowpass_noscale_norm_UMAP_diffday.png"), width=800, height=600)
-# DimPlot(sc, group.by="diffday")
-# dev.off()
-#
-# # UMAP individual
-# png(paste0(plots_loc, "lowpass_noscale_norm_UMAP_indiv.png"), width=800, height=600)
-# DimPlot(sc, group.by="individual")
-# dev.off()
-#
-# # UMAP collection
-# png(paste0(plots_loc, "lowpass_noscale_norm_UMAP_col.png"), width=800, height=600)
-# DimPlot(sc, group.by="orig.ident")
-# dev.off()
-#
-# # UMAP colday
-# png(paste0(plots_loc, "lowpass_noscale_norm_UMAP_colday.png"), width=800, height=600)
-# DimPlot(sc, group.by="colday")
-# dev.off()
-#
-# rm(list=ls())
-#
-# plots_loc <- "./seurat_plots/"
-#
-#
-# ### SCTransform
-# sc <- readRDS("./rds_objects/seurat_obj_lowpass_sctransform_processed.RDS")
-#
-# # PCA diffday
-# png(paste0(plots_loc, "lowpass_sctransform_PCA_diffday.png"), width=800, height=600)
-# PCAPlot(sc, group.by="diffday")
-# dev.off()
-#
-# # PCA individual
-# png(paste0(plots_loc, "lowpass_sctransform_PCA_indiv.png"), width=800, height=600)
-# PCAPlot(sc, group.by="individual")
-# dev.off()
-#
-# # PCA collection
-# png(paste0(plots_loc, "lowpass_sctransform_PCA_col.png"), width=800, height=600)
-# PCAPlot(sc, group.by="orig.ident")
-# dev.off()
-#
-# # PCA colday
-# png(paste0(plots_loc, "lowpass_sctransform_PCA_colday.png"), width=800, height=600)
-# PCAPlot(sc, group.by="colday")
-# dev.off()
-#
-# # UMAP diffday
-# png(paste0(plots_loc, "lowpass_sctransform_UMAP_diffday.png"), width=800, height=600)
-# DimPlot(sc, group.by="diffday")
-# dev.off()
-#
-# # UMAP individual
-# png(paste0(plots_loc, "lowpass_sctransform_UMAP_indiv.png"), width=800, height=600)
-# DimPlot(sc, group.by="individual")
-# dev.off()
-#
-# # UMAP collection
-# png(paste0(plots_loc, "lowpass_sctransform_UMAP_col.png"), width=800, height=600)
-# DimPlot(sc, group.by="orig.ident")
-# dev.off()
-#
-# # UMAP colday
-# png(paste0(plots_loc, "lowpass_sctransform_UMAP_colday.png"), width=800, height=600)
-# DimPlot(sc, group.by="colday")
-# dev.off()
-#
-# rm(list=ls())
+plots_loc <- "./seurat_plots/"
+
+### Basic norm
+sc <- readRDS("./rds_objects/seurat_obj_lowpass_basic_norm_processed.RDS")
+
+png(paste0(plots_loc, "lowpass_basic_norm_PCA_diffday.png"), width=800, height=600)
+PCAPlot(sc, group.by="diffday")
+dev.off()
+png(paste0(plots_loc, "lowpass_basic_norm_PCA_indiv.png"), width=800, height=600)
+PCAPlot(sc, group.by="individual")
+dev.off()
+png(paste0(plots_loc, "lowpass_basic_norm_PCA_col.png"), width=800, height=600)
+PCAPlot(sc, group.by="orig.ident")
+dev.off()
+png(paste0(plots_loc, "lowpass_basic_norm_PCA_colday.png"), width=800, height=600)
+PCAPlot(sc, group.by="colday")
+dev.off()
+
+png(paste0(plots_loc, "lowpass_basic_norm_UMAP_diffday.png"), width=800, height=600)
+DimPlot(sc, group.by="diffday")
+dev.off()
+png(paste0(plots_loc, "lowpass_basic_norm_UMAP_indiv.png"), width=800, height=600)
+DimPlot(sc, group.by="individual")
+dev.off()
+png(paste0(plots_loc, "lowpass_basic_norm_UMAP_col.png"), width=800, height=600)
+DimPlot(sc, group.by="orig.ident")
+dev.off()
+png(paste0(plots_loc, "lowpass_basic_norm_UMAP_colday.png"), width=800, height=600)
+DimPlot(sc, group.by="colday")
+dev.off()
+rm(list=ls())
+
+plots_loc <- "./seurat_plots/"
+### Noscale norm
+sc <- readRDS("./rds_objects/seurat_obj_lowpass_noscale_norm_processed.RDS")
+
+# PCA diffday
+png(paste0(plots_loc, "lowpass_noscale_norm_PCA_diffday.png"), width=800, height=600)
+PCAPlot(sc, group.by="diffday")
+dev.off()
+
+# PCA individual
+png(paste0(plots_loc, "lowpass_noscale_norm_PCA_indiv.png"), width=800, height=600)
+PCAPlot(sc, group.by="individual")
+dev.off()
+
+# PCA collection
+png(paste0(plots_loc, "lowpass_noscale_norm_PCA_col.png"), width=800, height=600)
+PCAPlot(sc, group.by="orig.ident")
+dev.off()
+
+# PCA colday
+png(paste0(plots_loc, "lowpass_noscale_norm_PCA_colday.png"), width=800, height=600)
+PCAPlot(sc, group.by="colday")
+dev.off()
+
+# UMAP diffday
+png(paste0(plots_loc, "lowpass_noscale_norm_UMAP_diffday.png"), width=800, height=600)
+DimPlot(sc, group.by="diffday")
+dev.off()
+
+# UMAP individual
+png(paste0(plots_loc, "lowpass_noscale_norm_UMAP_indiv.png"), width=800, height=600)
+DimPlot(sc, group.by="individual")
+dev.off()
+
+# UMAP collection
+png(paste0(plots_loc, "lowpass_noscale_norm_UMAP_col.png"), width=800, height=600)
+DimPlot(sc, group.by="orig.ident")
+dev.off()
+
+# UMAP colday
+png(paste0(plots_loc, "lowpass_noscale_norm_UMAP_colday.png"), width=800, height=600)
+DimPlot(sc, group.by="colday")
+dev.off()
+
+rm(list=ls())
+
+plots_loc <- "./seurat_plots/"
+
+
+### SCTransform
+sc <- readRDS("./rds_objects/seurat_obj_lowpass_sctransform_processed.RDS")
+
+# PCA diffday
+png(paste0(plots_loc, "lowpass_sctransform_PCA_diffday.png"), width=800, height=600)
+PCAPlot(sc, group.by="diffday")
+dev.off()
+
+# PCA individual
+png(paste0(plots_loc, "lowpass_sctransform_PCA_indiv.png"), width=800, height=600)
+PCAPlot(sc, group.by="individual")
+dev.off()
+
+# PCA collection
+png(paste0(plots_loc, "lowpass_sctransform_PCA_col.png"), width=800, height=600)
+PCAPlot(sc, group.by="orig.ident")
+dev.off()
+
+# PCA colday
+png(paste0(plots_loc, "lowpass_sctransform_PCA_colday.png"), width=800, height=600)
+PCAPlot(sc, group.by="colday")
+dev.off()
+
+# UMAP diffday
+png(paste0(plots_loc, "lowpass_sctransform_UMAP_diffday.png"), width=800, height=600)
+DimPlot(sc, group.by="diffday")
+dev.off()
+
+# UMAP individual
+png(paste0(plots_loc, "lowpass_sctransform_UMAP_indiv.png"), width=800, height=600)
+DimPlot(sc, group.by="individual")
+dev.off()
+
+# UMAP collection
+png(paste0(plots_loc, "lowpass_sctransform_UMAP_col.png"), width=800, height=600)
+DimPlot(sc, group.by="orig.ident")
+dev.off()
+
+# UMAP colday
+png(paste0(plots_loc, "lowpass_sctransform_UMAP_colday.png"), width=800, height=600)
+DimPlot(sc, group.by="colday")
+dev.off()
+
+rm(list=ls())
 #
 plots_loc <- "./seurat_plots/"
 
@@ -173,50 +173,50 @@ dev.off()
 
 rm(list=ls())
 
-# plots_loc <- "./seurat_plots/"
+plots_loc <- "./seurat_plots/"
 
 
-# ### SCTransform Mito Colday Regress
-# sc <- readRDS("./rds_objects/seurat_obj_lowpass_sctransform_mitocoldayregress_processed.RDS")
-#
-# # PCA diffday
-# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_diffday.png"), width=800, height=600)
-# PCAPlot(sc, group.by="diffday")
-# dev.off()
-#
-# # PCA individual
-# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_indiv.png"), width=800, height=600)
-# PCAPlot(sc, group.by="individual")
-# dev.off()
-#
-# # PCA collection
-# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_col.png"), width=800, height=600)
-# PCAPlot(sc, group.by="orig.ident")
-# dev.off()
-#
-# # PCA colday
-# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_colday.png"), width=800, height=600)
-# PCAPlot(sc, group.by="colday")
-# dev.off()
-#
-# # UMAP diffday
-# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_diffday.png"), width=800, height=600)
-# DimPlot(sc, group.by="diffday")
-# dev.off()
-#
-# # UMAP individual
-# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_indiv.png"), width=800, height=600)
-# DimPlot(sc, group.by="individual")
-# dev.off()
-#
-# # UMAP collection
-# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_col.png"), width=800, height=600)
-# DimPlot(sc, group.by="orig.ident")
-# dev.off()
-#
-# # UMAP colday
-# png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_colday.png"), width=800, height=600)
-# DimPlot(sc, group.by="colday")
-# dev.off()
-#
-# rm(list=ls())
+### SCTransform Mito Colday Regress
+sc <- readRDS("./rds_objects/seurat_obj_lowpass_sctransform_mitocoldayregress_processed.RDS")
+
+# PCA diffday
+png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_diffday.png"), width=800, height=600)
+PCAPlot(sc, group.by="diffday")
+dev.off()
+
+# PCA individual
+png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_indiv.png"), width=800, height=600)
+PCAPlot(sc, group.by="individual")
+dev.off()
+
+# PCA collection
+png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_col.png"), width=800, height=600)
+PCAPlot(sc, group.by="orig.ident")
+dev.off()
+
+# PCA colday
+png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_PCA_colday.png"), width=800, height=600)
+PCAPlot(sc, group.by="colday")
+dev.off()
+
+# UMAP diffday
+png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_diffday.png"), width=800, height=600)
+DimPlot(sc, group.by="diffday")
+dev.off()
+
+# UMAP individual
+png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_indiv.png"), width=800, height=600)
+DimPlot(sc, group.by="individual")
+dev.off()
+
+# UMAP collection
+png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_col.png"), width=800, height=600)
+DimPlot(sc, group.by="orig.ident")
+dev.off()
+
+# UMAP colday
+png(paste0(plots_loc, "lowpass_sctransform_mitocoldayregress_UMAP_colday.png"), width=800, height=600)
+DimPlot(sc, group.by="colday")
+dev.off()
+
+rm(list=ls())
--- bash_scripts/all_in_one.sh
+++ bash_scripts/all_in_one.sh
@@ -31,22 +31,19 @@ source_files="${22}"
 
 module load R
 
-srun Rscript ./R_scripts/create_seurat_object.R $cd1col1 $cd1col2 $cd1col3 $cd1col4 $cd1col5 $cd1col6 \
-                        $cd2col1 $cd2col2 $cd2col3 $cd2col4 $cd2col5 $cd2col6 \
-                        $cd3col1 $cd3col2 $cd3col3 $cd3col4 $cd3col5 $cd3col6 \
-                        $min_cells_per_gene $min_genes_per_cell $mito_threshold \
-                        $source_files 0
-wait
+# srun Rscript ./R_scripts/create_seurat_object.R $cd1col1 $cd1col2 $cd1col3 $cd1col4 $cd1col5 $cd1col6 \
+#                         $cd2col1 $cd2col2 $cd2col3 $cd2col4 $cd2col5 $cd2col6 \
+#                         $cd3col1 $cd3col2 $cd3col3 $cd3col4 $cd3col5 $cd3col6 \
+#                         $min_cells_per_gene $min_genes_per_cell $mito_threshold \
+#                         $source_files 0
+# wait
 srun Rscript ./R_scripts/seurat_normalization.R $source_files 1
 wait
-# srun Rscript ./R_scripts/seurat2monocle.R 2
-# wait
-# srun Rscript ./R_scripts/seurat_dimreduc.R  3 &
-# srun Rscript ./R_scripts/monocle_dimreduc.R 4
-# wait
+srun Rscript ./R_scripts/seurat2monocle.R 2
+wait
+srun Rscript ./R_scripts/seurat_dimreduc.R  3 &
+srun Rscript ./R_scripts/monocle_dimreduc.R 4
+wait
 # srun Rscript ./R_scripts/seurat_visualizations.R 5 &
 # srun Rscript ./R_scripts/monocle_visualizations.R 6
 # wait
-Rscript ./R_scripts/seurat_dimreduc.R 2
-wait
-Rscript ./R_scripts/seurat_visualizations.R 3
