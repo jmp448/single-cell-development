d28eccf28f62aee0f7d9e21c6a8e2d709c1f7e1a
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-11-05 14:52:49 -0500

    Added proper args to all, fixed some paste0 errors

--- R_scripts/monocle_dimreduc.R
+++ R_scripts/monocle_dimreduc.R
@@ -27,10 +27,10 @@ source_files = args[1]
 # saveRDS(m, "./rds_objects/monocle_from_seurat_lowpass_sctransform_processed.RDS")
 # rm(list=ls())
 
-m <- readRDS("./rds_objects/monocle_from_seurat_", source_files, "_sctransform_mitoregress.RDS")
+m <- readRDS(paste0("./rds_objects/monocle_from_seurat_", source_files, "_sctransform_mitoregress.RDS"))
 m <- preprocess_cds(m, method="PCA", norm_method="none", scaling=F)
 m <- reduce_dimension(m, reduction_method="UMAP", preprocess_method="PCA", umap.min_dist=0.1, umap.n_neighbors=30L, umap.fast_sgd=F)
-saveRDS(m, "./rds_objects/monocle_from_seurat_", source_files, "_sctransform_mitoregress_processed.RDS")
+saveRDS(m, paste0("./rds_objects/monocle_from_seurat_", source_files, "_sctransform_mitoregress_processed.RDS"))
 rm(list=ls())
 #
 # m <- readRDS("./rds_objects/monocle_from_seurat_lowpass_sctransform_mitocoldayregress.RDS")
