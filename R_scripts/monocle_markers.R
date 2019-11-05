f913dbd9613cbacfd480e6f41e73ae23078adc5b
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-11-05 17:02:59 -0500

    Update monocle markers

--- R_scripts/monocle_markers.R
+++ R_scripts/monocle_markers.R
@@ -2,12 +2,12 @@
 
 library(monocle3)
 
-cds <- readRDS("./rds_objects/monocle_from_seurat_lowpass_sctransform_mitoregress_processed.RDS")
+cds <- readRDS("./rds_objects/monocle_from_seurat_fulldata_sctransform_mitoregress_processed.RDS")
 
 # Check out expression for specific genes
 # Stem cell markers
 stem_cell_markers <- c("EPCAM")
-png("./monocle_plots/markers_stem_cell.png", width=800, height=600)
+png("./monocle_plots/fulldata_markers_stem_cell.png", width=800, height=600)
 plot_cells(cds, genes=stem_cell_markers,
            label_cell_groups=FALSE,
            show_trajectory_graph=FALSE)
@@ -15,7 +15,7 @@ dev.off()
 
 # Mesoderm
 mesoderm_markers <- c("MIXL1")
-png("./monocle_plots/markers_mesoderm.png", width=800, height=600)
+png("./monocle_plots/fulldata_markers_mesoderm.png", width=800, height=600)
 plot_cells(cds, genes=mesoderm_markers,
            label_cell_groups=FALSE,
            show_trajectory_graph=FALSE)
@@ -23,7 +23,7 @@ dev.off()
 
 # Cardiomyocyte
 cardiomyocyte_markers <- c("TNNT2", "TNNI1", "TNNC1")
-png("./monocle_plots/markers_cardiomyocyte.png", width=1800, height=600)
+png("./monocle_plots/fulldata_markers_cardiomyocyte.png", width=1800, height=600)
 plot_cells(cds, genes=cardiomyocyte_markers,
            label_cell_groups=FALSE,
            show_trajectory_graph=FALSE)
@@ -31,7 +31,7 @@ dev.off()
 
 # Epicardium
 epicardium_markers = "IGF2"
-png("./monocle_plots/markers_epicardium.png", width=800, height=600)
+png("./monocle_plots/fulldata_markers_epicardium.png", width=800, height=600)
 plot_cells(cds, genes=epicardium_markers,
            label_cell_groups=FALSE,
            show_trajectory_graph=FALSE)
@@ -39,7 +39,7 @@ dev.off()
 
 # Fibroblast
 fibroblast_markers <- c("VIM", "S100A4", "COL1A2", "COL3A1", "TSLP")
-png("./monocle_plots/fibroblast_markers.png", width=1800, height=600)
+png("./monocle_plots/fulldata_markers_fibroblast.png", width=1800, height=600)
 plot_cells(cds, genes=fibroblast_markers,
            label_cell_groups=FALSE,
            show_trajectory_graph=FALSE)
