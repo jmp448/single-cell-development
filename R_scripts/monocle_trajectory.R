f0ecf8797ac8238711c6d75ffef2901db4507abc
Joshua Popp <jpopp@midway2-login2.rcc.local>
2019-11-05 15:07:18 -0600

    Fixes to monocle_trajectory

--- R_scripts/monocle_trajectory.R
+++ R_scripts/monocle_trajectory.R
@@ -1,18 +1,23 @@
 library(monocle3)
 
-cds <- readRDS("./rds_objects/monocle_from_seurat_lowpass_sctransform_mitoregress_processed.RDS")
+cds <- readRDS("./rds_objects/monocle_from_seurat_fulldata_sctransform_mitoregress_processed.RDS")
 
-library(monocle3)
-
-cds=cluster_cells(cds, resolution=1e-7)
+cds <- cluster_cells(cds, resolution=1e-7)
 plt <- plot_cells(cds)
 print(plt)
 plt <- plot_cells(cds, color_cells_by="partition", group_cells_by="partition")
 print(plt)
 cds <- learn_graph(cds)
-plot_cells(cds,
+cds <- order_cells(cds)
+plt <- plot_cells(cds,
            color_cells_by = "diffday",
-           label_groups_by_cluster=F,
+           label_cell_groups=F,
            label_leaves=F,
            label_branch_points=F)
-cds <- order_cells(cds)
+print(plt)
+plt <- plot_cells(cds,
+           color_cells_by = "pseudotime",
+           label_cell_groups=F,
+           label_leaves=F,
+           label_branch_points=F)
+print(plt)
