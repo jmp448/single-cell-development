ee84a0802e22f4bab83844562930f07ed33b09ba
Joshua Popp <jpopp@midway2-login2.rcc.local>
2019-11-06 20:15:59 -0600

    Update script for monocle and seurat plots

--- R_scripts/monocle_scatterplots.R
+++ R_scripts/monocle_scatterplots.R
@@ -1,8 +1,10 @@
 library(ggplot2)
+library(monocle3)
 
 cds <- readRDS("./rds_objects/monocle_from_seurat_fulldata_sctransform_mitoregress_graphed.RDS")
 
-plt <- ggplot(as.data.frame(cds@colData), aes(x=pseudotime(cds), y=cds$diffday)) + 
-  geom_point(aes(color=cds@assays[["counts"]]["TNNT2",]))
+plt <- ggplot(as.data.frame(cds@colData), aes(x=pseudotime(cds), y=cds$diffday, color=cds@assays[["counts"]]["TNNT2",], size=cds@assays[["counts"]]["TNNT2",])) + 
+  geom_point() +
+  scale_color_gradient(low="white", high="red")
 
 print(plt)
